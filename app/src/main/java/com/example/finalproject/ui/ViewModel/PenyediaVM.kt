ba247c016e8ade705b32689ea9c3fb6a9a039612
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-19 01:44:05 +0700

    membuat penyedia viewmodel

---
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/PenyediaVM.kt
@@ -0,0 +1,16 @@
+package com.example.finalproject.ui.ViewModel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeMhsVM(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertMhsVM(aplikasiMahasiswa().container.mahasiswaRepository) }
+
+    }
+}
+fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
