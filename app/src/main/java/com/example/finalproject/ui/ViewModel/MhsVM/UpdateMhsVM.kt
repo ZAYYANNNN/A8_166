7df99925b051a19a1186bd1f95d3fcbe7ad4085f
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-19 23:50:24 +0700

    membuat update view model mahasiswa

---
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/MhsVM/UpdateMhsVM.kt
@@ -0,0 +1,52 @@
+package com.example.finalproject.ui.ViewModel.MhsVM
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject.Model.Mahasiswa
+import com.example.finalproject.Repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class UpdateMhsVM(
+    savedStateHandle: SavedStateHandle,
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+
+    val idMahasiswa: String = checkNotNull(savedStateHandle[DestinasiUpdateMhs.NIM])
+    var uiState = mutableStateOf(MahasiswaUiState())
+        private set
+    init {
+        ambilMahasiswa()
+    }
+    // Fetch the student data using NIM
+    private fun ambilMahasiswa() {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswabyid(idMahasiswa)
+                mahasiswa?.let {
+                    uiState.value = it.toMahasiswaUIEvent() // Update state with the fetched data
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    // Update the mahasiswa information
+    fun updateMhs(idMahasiswa: String, mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(idMahasiswa, mahasiswa)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    // Update the UI state with a new InsertUiEvent
+    fun updateMhsState(mahasiswaUiEvent: MahasiswaUiEvent) {
+        uiState.value = uiState.value.copy(mahasiswaUiEvent = mahasiswaUiEvent)
+    }
+}
+fun Mahasiswa.toMahasiswaUIEvent(): MahasiswaUiState = MahasiswaUiState(
+    mahasiswaUiEvent = this.toDetailMhsUiEvent()
+)
