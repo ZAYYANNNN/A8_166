cf33c17418695378081e9caa2035f3cb5c968bb2
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-21 15:30:13 +0700

    menambah penyedia view model untuk bangunan dan pembayaran

--- app/src/main/java/com/example/finalproject/ui/ViewModel/PenyediaVM.kt
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/PenyediaVM.kt
@@ -5,8 +5,14 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.finalproject.BangunanApplications
 import com.example.finalproject.KamarApplications
 import com.example.finalproject.MahasiswaApplications
+import com.example.finalproject.SewaApplications
+import com.example.finalproject.ui.ViewModel.BangunanVM.DetailBangunanVM
+import com.example.finalproject.ui.ViewModel.BangunanVM.HomeBangunanVM
+import com.example.finalproject.ui.ViewModel.BangunanVM.InsertBangunanVM
+import com.example.finalproject.ui.ViewModel.BangunanVM.UpdateBangunanVM
 import com.example.finalproject.ui.ViewModel.KamarVM.DetailKamarVM
 import com.example.finalproject.ui.ViewModel.KamarVM.HomeKamarVM
 import com.example.finalproject.ui.ViewModel.KamarVM.InsertKamarVM
@@ -15,6 +21,10 @@ import com.example.finalproject.ui.ViewModel.MhsVM.DetailMhsVM
 import com.example.finalproject.ui.ViewModel.MhsVM.HomeMhsVM
 import com.example.finalproject.ui.ViewModel.MhsVM.InsertMhsVM
 import com.example.finalproject.ui.ViewModel.MhsVM.UpdateMhsVM
+import com.example.finalproject.ui.ViewModel.PayVM.DetailPayVM
+import com.example.finalproject.ui.ViewModel.PayVM.HomePayVM
+import com.example.finalproject.ui.ViewModel.PayVM.InsertPayVM
+import com.example.finalproject.ui.ViewModel.PayVM.UpdatePayVM
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
@@ -28,6 +38,16 @@ object PenyediaViewModel{
         initializer { DetailKamarVM(createSavedStateHandle(),aplikasiKamar().container.kamarRepository) }
         initializer { UpdateKamarVM(createSavedStateHandle(),aplikasiKamar().container.kamarRepository) }
 
+        initializer { HomeBangunanVM(aplikasiBangunan().container.bangunanRepository) }
+        initializer { InsertBangunanVM(aplikasiBangunan().container.bangunanRepository) }
+        initializer { DetailBangunanVM(createSavedStateHandle(),aplikasiBangunan().container.bangunanRepository) }
+        initializer { UpdateBangunanVM(createSavedStateHandle(),aplikasiBangunan().container.bangunanRepository) }
+
+        initializer { HomePayVM(aplikasiSewa().container.sewaRepository) }
+        initializer { InsertPayVM(aplikasiSewa().container.sewaRepository) }
+        initializer { DetailPayVM(createSavedStateHandle(),aplikasiSewa().container.sewaRepository) }
+        initializer { UpdatePayVM(createSavedStateHandle(),aplikasiSewa().container.sewaRepository) }
+
     }
 }
 fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
@@ -35,3 +55,9 @@ fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
 
 fun CreationExtras.aplikasiKamar():KamarApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KamarApplications)
+
+fun CreationExtras.aplikasiBangunan():BangunanApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as BangunanApplications)
+
+fun CreationExtras.aplikasiSewa(): SewaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as SewaApplications)
