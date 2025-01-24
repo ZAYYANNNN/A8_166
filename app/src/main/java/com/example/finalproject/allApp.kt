f88dd9558f903613647e208f03c72f0d9bbe9663
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-24 14:00:22 +0700

    mengubah kode allapp

--- app/src/main/java/com/example/finalproject/allApp.kt
+++ app/src/main/java/com/example/finalproject/allApp.kt
@@ -1,41 +1,13 @@
 package com.example.finalproject
 
 import android.app.Application
-import com.example.finalproject.ui.Container.BangunanContainer
-import com.example.finalproject.ui.Container.BgnContainer
-import com.example.finalproject.ui.Container.KamarContainer
-import com.example.finalproject.ui.Container.KmrContainer
-import com.example.finalproject.ui.Container.MahasiswaContainer
-import com.example.finalproject.ui.Container.MhsContainer
-import com.example.finalproject.ui.Container.PayContainer
-import com.example.finalproject.ui.Container.SewaContainer
+import com.example.finalproject.ui.Container.AppContainer
+import com.example.finalproject.ui.Container.StayContainer
 
-class MahasiswaApplications: Application()
-{ lateinit var container: MhsContainer
+class StayApplications: Application()
+{ lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = MahasiswaContainer()
-    }
-}
-
-class KamarApplications: Application()
-{ lateinit var container: KmrContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = KamarContainer()
-    }
-}
-class BangunanApplications: Application()
-{ lateinit var container: BgnContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = BangunanContainer()
-    }
-}
-class SewaApplications: Application()
-{ lateinit var container: PayContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = SewaContainer()
+        container = StayContainer()
     }
 }
