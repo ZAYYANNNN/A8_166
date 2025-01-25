15a97f4ac4771752b51e3a8b6dd2367870b5e177
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-25 14:58:39 +0700

    menambah serialname untuk inisialisasi di backend

--- app/src/main/java/com/example/finalproject/Model/Mahasiswa.kt
+++ app/src/main/java/com/example/finalproject/Model/Mahasiswa.kt
@@ -5,11 +5,23 @@ import kotlinx.serialization.*
 @OptIn(InternalSerializationApi::class)
 @Serializable
 data class Mahasiswa(
+
+    @SerialName("id_mahasiswa")
     val idMahasiswa : String,
+
+    @SerialName("nama_mahasiswa")
     val nama : String,
+
+    @SerialName("nomor_identitas")
     val nim : String,
+
+    @SerialName("email")
     val email: String,
+
+    @SerialName("nomor_telepon")
     val telp: String,
+
+    @SerialName("id_kamar")
     val idKamar : String
 
 )
