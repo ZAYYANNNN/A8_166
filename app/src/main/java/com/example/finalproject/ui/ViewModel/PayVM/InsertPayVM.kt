6eed9066811bc23616b80966512829d3185f5311
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-25 23:56:47 +0700

    membuat variabel mahasiswalist

--- app/src/main/java/com/example/finalproject/ui/ViewModel/PayVM/InsertPayVM.kt
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/PayVM/InsertPayVM.kt
@@ -7,25 +7,47 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.finalproject.Model.Mahasiswa
 import com.example.finalproject.Model.Sewa
+import com.example.finalproject.Repository.MahasiswaRepository
 import com.example.finalproject.Repository.SewaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class InsertPayVM(private val swa: SewaRepository) : ViewModel(){
+class InsertPayVM(
+    private val swa: SewaRepository,
+    private val mhs: MahasiswaRepository
+) : ViewModel(){
 
     var uiState by mutableStateOf(SewaUiState())
         private set
 
+    // StateFlow untuk menyimpan daftar kamar yang diambil dari repository
+    private val _mahasiswaList = MutableStateFlow<List<Mahasiswa>>(emptyList())
+    val mahasiswaList: StateFlow<List<Mahasiswa>> get() = _mahasiswaList
 
-    fun updateInsertSwaState(sewaUiEvent: SewaUiEvent) {
-        uiState = SewaUiState(sewaUiEvent = sewaUiEvent)
+    fun loadMahasiswaList() {
+        viewModelScope.launch {
+            try {
+                val response = mhs.getMahasiswa()
+                _mahasiswaList.value = response.data
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
 
-    suspend fun insertSwa() {
+    fun updateInsertSwaState(sewaUiEvent: SewaUiEvent) {
+        uiState = uiState.copy(sewaUiEvent = sewaUiEvent)
+    }
+
+
+    fun insertSwa() {
         viewModelScope.launch {
             try {
-                swa.insertPembayaran(uiState.sewaUiEvent.toSwa())
-            }catch (e:Exception){
+                val sewa = uiState.sewaUiEvent.toSwa()
+                swa.insertPembayaran(sewa)
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
@@ -33,8 +55,8 @@ class InsertPayVM(private val swa: SewaRepository) : ViewModel(){
 }
 
 data class SewaUiState(
-    val sewaUiEvent:SewaUiEvent = SewaUiEvent(),
-    val mahasiswaList:List<Mahasiswa> = emptyList()
+    val sewaUiEvent: SewaUiEvent = SewaUiEvent(),
+    val mahasiswaList: List<Mahasiswa> = emptyList()
 )
 
 data class SewaUiEvent(
@@ -45,22 +67,10 @@ data class SewaUiEvent(
     val statusPembayaran:String = "",
 )
 
-fun SewaUiEvent.toSwa():Sewa = Sewa(
+fun SewaUiEvent.toSwa(): Sewa = Sewa(
     idPembayaran = idPembayaran,
     idMahasiswa = idMahasiswa,
     jumlah = jumlah,
     tglPembayaran = tglPembayaran,
-    statusPembayaran= statusPembayaran
-)
-
-fun Sewa.toUiStateSewa():SewaUiState = SewaUiState(
-    sewaUiEvent = toSewaUiEvent()
-)
-
-fun Sewa.toSewaUiEvent(): SewaUiEvent = SewaUiEvent(
-    idPembayaran = idPembayaran,
-    idMahasiswa = idMahasiswa,
-    jumlah = jumlah,
-    tglPembayaran = tglPembayaran,
-    statusPembayaran= statusPembayaran
+    statusPembayaran = statusPembayaran
 )
