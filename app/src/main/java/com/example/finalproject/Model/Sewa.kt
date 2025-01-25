9531c7f332e6a30e7978a5ed70f1c05d262c4244
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-25 23:36:38 +0700

    menambah serial name untuk sewa

--- app/src/main/java/com/example/finalproject/Model/Sewa.kt
+++ app/src/main/java/com/example/finalproject/Model/Sewa.kt
@@ -1,15 +1,26 @@
 package com.example.finalproject.Model
 
 import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @OptIn(InternalSerializationApi::class)
 @Serializable
 data class Sewa(
+
+    @SerialName("id_pembayaran")
     val idPembayaran : String,
+
+    @SerialName("id_mahasiswa")
     val idMahasiswa : String,
+
+    @SerialName("jumlah")
     val jumlah : String,
+
+    @SerialName("tgl_pembayaran")
     val tglPembayaran: String,
+
+    @SerialName("status_pembayaran")
     val statusPembayaran : String
 
 )
