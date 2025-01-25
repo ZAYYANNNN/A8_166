1480d6fe26077eeda83d94ee311034c407f7a14e
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-25 23:32:20 +0700

    menambah mahasiswarepo pada penyedia vm sewa

--- app/src/main/java/com/example/finalproject/ui/ViewModel/PenyediaVM.kt
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/PenyediaVM.kt
@@ -5,10 +5,7 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.finalproject.BangunanApplications
-import com.example.finalproject.KamarApplications
-import com.example.finalproject.MahasiswaApplications
-import com.example.finalproject.SewaApplications
+import com.example.finalproject.StayApplications
 import com.example.finalproject.ui.ViewModel.BangunanVM.DetailBangunanVM
 import com.example.finalproject.ui.ViewModel.BangunanVM.HomeBangunanVM
 import com.example.finalproject.ui.ViewModel.BangunanVM.InsertBangunanVM
@@ -26,38 +23,33 @@ import com.example.finalproject.ui.ViewModel.PayVM.HomePayVM
 import com.example.finalproject.ui.ViewModel.PayVM.InsertPayVM
 import com.example.finalproject.ui.ViewModel.PayVM.UpdatePayVM
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeMhsVM(aplikasiMahasiswa().container.mahasiswaRepository) }
-        initializer { InsertMhsVM(aplikasiMahasiswa().container.mahasiswaRepository) }
-        initializer { DetailMhsVM(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
-        initializer { UpdateMhsVM(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
+        // ViewModel untuk Mahasiswa
+        initializer { HomeMhsVM(aplikasiStay().container.mahasiswaRepository) }
+        initializer { InsertMhsVM(aplikasiStay().container.mahasiswaRepository, aplikasiStay().container.kamarRepository) }  // Tambahkan KamarRepository
+        initializer { DetailMhsVM(createSavedStateHandle(), aplikasiStay().container.mahasiswaRepository) }
+        initializer { UpdateMhsVM(createSavedStateHandle(), aplikasiStay().container.mahasiswaRepository, aplikasiStay().container.kamarRepository) }
 
-        initializer { HomeKamarVM(aplikasiKamar().container.kamarRepository) }
-        initializer { InsertKamarVM(aplikasiKamar().container.kamarRepository) }
-        initializer { DetailKamarVM(createSavedStateHandle(),aplikasiKamar().container.kamarRepository) }
-        initializer { UpdateKamarVM(createSavedStateHandle(),aplikasiKamar().container.kamarRepository) }
+        // ViewModel untuk Kamar
+        initializer { HomeKamarVM(aplikasiStay().container.kamarRepository) }
+        initializer { InsertKamarVM(aplikasiStay().container.kamarRepository, aplikasiStay().container.bangunanRepository) }
+        initializer { DetailKamarVM(createSavedStateHandle(), aplikasiStay().container.kamarRepository) }
+        initializer { UpdateKamarVM(createSavedStateHandle(), aplikasiStay().container.kamarRepository, aplikasiStay().container.bangunanRepository) }
 
-        initializer { HomeBangunanVM(aplikasiBangunan().container.bangunanRepository) }
-        initializer { InsertBangunanVM(aplikasiBangunan().container.bangunanRepository) }
-        initializer { DetailBangunanVM(createSavedStateHandle(),aplikasiBangunan().container.bangunanRepository) }
-        initializer { UpdateBangunanVM(createSavedStateHandle(),aplikasiBangunan().container.bangunanRepository) }
+        // ViewModel untuk Bangunan
+        initializer { HomeBangunanVM(aplikasiStay().container.bangunanRepository) }
+        initializer { InsertBangunanVM(aplikasiStay().container.bangunanRepository) }
+        initializer { DetailBangunanVM(createSavedStateHandle(), aplikasiStay().container.bangunanRepository) }
+        initializer { UpdateBangunanVM(createSavedStateHandle(), aplikasiStay().container.bangunanRepository) }
 
-        initializer { HomePayVM(aplikasiSewa().container.sewaRepository) }
-        initializer { InsertPayVM(aplikasiSewa().container.sewaRepository) }
-        initializer { DetailPayVM(createSavedStateHandle(),aplikasiSewa().container.sewaRepository) }
-        initializer { UpdatePayVM(createSavedStateHandle(),aplikasiSewa().container.sewaRepository) }
-
+        // ViewModel untuk Pembayaran
+        initializer { HomePayVM(aplikasiStay().container.sewaRepository) }
+        initializer { InsertPayVM(aplikasiStay().container.sewaRepository, aplikasiStay().container.mahasiswaRepository) }
+        initializer { DetailPayVM(createSavedStateHandle(), aplikasiStay().container.sewaRepository) }
+        initializer { UpdatePayVM(createSavedStateHandle(), aplikasiStay().container.sewaRepository, aplikasiStay().container.mahasiswaRepository) }
     }
 }
-fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
 
-fun CreationExtras.aplikasiKamar():KamarApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KamarApplications)
-
-fun CreationExtras.aplikasiBangunan():BangunanApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as BangunanApplications)
-
-fun CreationExtras.aplikasiSewa(): SewaApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as SewaApplications)
+fun CreationExtras.aplikasiStay(): StayApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as StayApplications)
