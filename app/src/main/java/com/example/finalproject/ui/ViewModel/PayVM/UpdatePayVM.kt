9ac66d86b4bb8e79d93b7eddb087d3c3afe50f8d
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2025-01-26 00:01:23 +0700

    membuat fungsi loadmahasiswalist

--- app/src/main/java/com/example/finalproject/ui/ViewModel/PayVM/UpdatePayVM.kt
+++ app/src/main/java/com/example/finalproject/ui/ViewModel/PayVM/UpdatePayVM.kt
@@ -4,21 +4,29 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.finalproject.Model.Mahasiswa
 import com.example.finalproject.Model.Sewa
+import com.example.finalproject.Repository.MahasiswaRepository
 import com.example.finalproject.Repository.SewaRepository
 import com.example.finalproject.ui.View.PayView.DestinasiUpdatePay
+
 import kotlinx.coroutines.launch
 
 class UpdatePayVM(
     savedStateHandle: SavedStateHandle,
-    private val swa: SewaRepository
+    private val swa: SewaRepository,
+    private val mhs: MahasiswaRepository
 ) : ViewModel() {
 
     val idPembayaran: String = checkNotNull(savedStateHandle[DestinasiUpdatePay.IDPay])
     var uiState = mutableStateOf(SewaUiState())
         private set
+
+    var mahasiswaList = mutableStateOf<List<Mahasiswa>>(emptyList())
+
     init {
         ambilSewa()
+        loadMahasiswaList()
     }
 
     private fun ambilSewa() {
@@ -26,8 +34,18 @@ class UpdatePayVM(
             try {
                 val sewa = swa.getPembayaranbyid(idPembayaran)
                 sewa?.let {
-                    uiState.value = it.toSewaUIEvent() // Update state with the fetched data
+                    uiState.value = it.toSewaUIEvent()
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun loadMahasiswaList() {
+        viewModelScope.launch {
+            try {
+                val response = mhs.getMahasiswa()
+                mahasiswaList.value = response.data
             } catch (e: Exception) {
                 e.printStackTrace()
             }
